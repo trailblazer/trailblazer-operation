6259343ce06a5f164f84c70576b0e8d03cb7ffe6
Nick Sutterer <apotonick@gmail.com>
2016-09-30 13:50:57 +0800

    make Op#invalid! work.

--- lib/trailblazer/operation.rb
+++ lib/trailblazer/operation.rb
@@ -13,8 +13,8 @@ module Trailblazer
       @valid = true
     end
 
-    def call(**) # receives args[:params]
-      result(process)#(*)
+    def call(**args) # receives args[:params]
+      result(process(args))#(*)
     end
 
   private
@@ -25,8 +25,30 @@ module Trailblazer
     def result(returned, **)
       { valid: @valid, operation: self }#.merge(returned)
     end
+
+    # DISCUSS: do we want that per default?
+    module State
+      module Valid
+        def invalid!
+          @valid = false
+        end
+      end
+    end
+    include State::Valid # #invalid! - should we have that per default?
   end
 end
 
 # initialize: @result = {}
 # call -> merge .process
+
+# per default, an operation has a binary result: success/invalid
+# an attempt to cleanup before 2.0 with pipetree
+
+# TODO:
+# Deprecation::Run (old semantics!)
+# Make ::builds work "anywhere", without Op interface
+
+# CHANGES:
+# * Removed `Operation::[]` in favor of `Operation::()`.
+# * `Operation#invalid!` doesn't accept a result anymore.
+# * Removed `Operation#valid?` in favor of the result object.
--- test/call_test.rb
+++ test/call_test.rb
@@ -8,4 +8,16 @@ class CallTest < Minitest::Spec
     # in 1.2, ::() returns op instance.
     it { Create.()[:operation].must_be_instance_of Create }
   end
+
+  describe "#invalid!" do
+    class Delete < Trailblazer::Operation
+      def process(invalid:, **params)
+        invalid! if invalid
+      end
+    end
+
+    it { Delete.(params: {invalid: false})[:valid].must_equal true }
+    it { Delete.(params: {invalid: true})[:valid].must_equal false }
+  end
 end
+
