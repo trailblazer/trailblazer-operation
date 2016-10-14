d0b19dc31d4c25c996eb2be66df03ce882bb7879
Nick Sutterer <apotonick@gmail.com>
2016-10-15 10:20:23 +1100

    add test to verify we can simply use a dry-container (or any container with a #[] interface) as instance_attrs, if we wanted that.

---
+++ test/dry_container_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+require "dry/container"
+
+class DryContainerTest < Minitest::Spec
+  my_container = Dry::Container.new
+  my_container.register("user_repository", -> { Object })
+  my_container.namespace("contract") do
+    register("create") { Array }
+  end
+
+  class Create < Trailblazer::Operation
+  end
+
+  it { Create.({}, my_container)[:operation]["user_repository"].must_equal Object }
+  it { Create.({}, my_container)[:operation]["contract.create"].must_equal Array }
+end
+
+
