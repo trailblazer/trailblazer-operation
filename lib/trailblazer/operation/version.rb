a6bbddae9f9e8514fa6e1e35dd3c3e40ecaa1d84
Nick Sutterer <apotonick@gmail.com>
2016-11-10 18:17:07 +0800

    releasing 0.0.2 NOW.

--- lib/trailblazer/operation/version.rb
+++ lib/trailblazer/operation/version.rb
@@ -2,6 +2,6 @@ module Trailblazer
   # The Trailblazer-style operation.
   # Note that you don't have to use our "opinionated" version with result object, skills, etc.
   class Operation
-    VERSION = "2.0.0"
+    VERSION = "0.0.2"
   end
 end
