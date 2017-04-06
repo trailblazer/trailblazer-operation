3e8524c18814074e7ca18facc05597dd29606cd1
Nick Sutterer <apotonick@gmail.com>
2017-04-06 15:45:25 +0800

    test all combinations of overriding :name in #step.

--- test/step_test.rb
+++ test/step_test.rb
@@ -4,6 +4,7 @@ require "test_helper"
 # ---  step ->(*) { snippet }
 # ---  step Callable
 # ---  step :method
+# ---  step MyMacro
 class StepTest < Minitest::Spec
   class Callable
     def self.call(options, b:nil, **)
@@ -34,5 +35,18 @@ class StepTest < Minitest::Spec
 
   it { Create.({}, a: 1, b: 2, c: 3, d: 4, e: 5).inspect("a", "b", "c", "d", "e").must_equal "<Result:true [1, 2, 3, 4, 5] >" }
 
-  it { Trailblazer::Operation::Inspect.call(Create).gsub(/0x[\w]+/, "").must_equal %{[>#<Proc:@test/step_test.rb:24 (lambda)>,>StepTest::Callable,>#<Method: StepTest::Implementation.c>,>d,>]} }
+  it { Trailblazer::Operation::Inspect.call(Create).gsub(/0x[\w]+/, "").must_equal %{[>#<Proc:@test/step_test.rb:25 (lambda)>,>StepTest::Callable,>#<Method: StepTest::Implementation.c>,>d,>]} }
+
+  #---
+  #- step :whatever, name: :validate
+  #- step MyMacro, name: :validate
+  #- step [MyMacro, name: :validate], name: "Best!"
+  class Index < Trailblazer::Operation
+    step :validate!, name: "my validate"
+    step :persist!
+    step [ MyMacro, name: "I win!" ]
+    step [ MyMacro, name: "I win!" ], name: "No, I do!"
+  end
+
+  it { Trailblazer::Operation::Inspect.call(Index).must_equal %{[>my validate,>persist!,>I win!,>No, I do!]} }
 end
