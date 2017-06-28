a8f24f0ab93a1146d0f2daa319239c199bb3e05c
Nick Sutterer <apotonick@gmail.com>
2017-06-28 12:47:49 +1000

    use circuit 0.0.8.

--- lib/trailblazer/operation/task_wrap.rb
+++ lib/trailblazer/operation/task_wrap.rb
@@ -20,21 +20,29 @@ module Trailblazer
         def __call__(direction, options, flow_options={}) # FIXME: direction
           activity     = self["__activity__"]
 
-          # TODO: we can probably save a lot of time here by using constants.
-          wrap_static  = Circuit::Wrap::Alterations.new( map: self["__task_wraps__"] )
-          wrap_runtime = Circuit::Wrap::Alterations.new
-
-          # override:
-          flow_options = flow_options.merge(
-            runner:      Circuit::Wrap::Runner,
-            wrap_static: wrap_static,
-            debug:       activity.circuit.instance_variable_get(:@name)
-          )
-          # reverse_merge:
-          flow_options = { wrap_runtime: wrap_runtime }.merge(flow_options)
+          options, flow_options = TaskWrap.arguments_for_call(self, direction, options, flow_options)
 
           super(activity[:Start], options, flow_options) # Railway::__call__
         end
+      end
+
+      def self.arguments_for_call(operation, direction, options, flow_options)
+        activity     = operation["__activity__"]
+
+        # TODO: we can probably save a lot of time here by using constants.
+        wrap_static  = Circuit::Wrap::Alterations.new( map: operation["__task_wraps__"] )
+        wrap_runtime = Circuit::Wrap::Alterations.new
+
+        # override:
+        flow_options = flow_options.merge(
+          runner:      Circuit::Wrap::Runner,
+          wrap_static: wrap_static,
+          debug:       activity.circuit.instance_variable_get(:@name)
+        )
+        # reverse_merge:
+        flow_options = { wrap_runtime: wrap_runtime }.merge(flow_options)
+
+        [ options, flow_options ]
       end
 
       module DSL
@@ -50,3 +58,6 @@ module Trailblazer
     end # TaskWrap
   end
 end
+
+
+# |-- Railway::Call "insert.exec_context"
