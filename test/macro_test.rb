b20a242514bb5438af309b2b35f22fec158dac33
Nick Sutterer <apotonick@gmail.com>
2017-08-08 09:52:11 +0200

    :fireworks: wire one of the macro's ends to the pass_fast edge. HOW COOL IS THAT?????

--- test/macro_test.rb
+++ test/macro_test.rb
@@ -13,7 +13,7 @@ class MacroTest < Minitest::Spec
 
   class Create < Trailblazer::Operation
     step :a
-    step [ MacroB, { name: :MacroB }, {}, { "Allgood" => { role: :success }, "Fail!" => { role: :failure } } ]
+    step [ MacroB, { name: :MacroB }, {}, { "Allgood" => { role: :success }, "Fail!" => { role: :failure }, "Winning" => { role: :pass_fast } } ]
     step :c
 
     def a(options, **); options[:a] = true end
@@ -24,4 +24,6 @@ class MacroTest < Minitest::Spec
   it { Create.( {}, MacroB_return: "Allgood" ).inspect(:a, :B, :c).must_equal %{<Result:true [true, true, true] >} }
   # MacroB returns Fail! and is wired to the :failure edge (left track).
   it { Create.( {}, MacroB_return: "Fail!" ).inspect(:a, :B, :c).must_equal %{<Result:false [true, true, nil] >} }
+  # MacroB returns Winning and is wired to the :pass_fast edge.
+  it { Create.( {}, MacroB_return: "Winning" ).inspect(:a, :B, :c).must_equal %{<Result:true [true, true, nil] >} }
 end
