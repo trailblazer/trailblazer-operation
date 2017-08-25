3e5831ec0f9f85221e6d19098d26e2b88bac607b
Nick Sutterer <apotonick@gmail.com>
2017-08-25 13:09:14 +0200

    start drafting the API for DSL::task, which let's you wire a task to a track, but allows rewiring its outputs, which is pretty awesome.

---
+++ test/wire_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class WireTest < Minitest::Spec
+  Circuit = Trailblazer::Circuit
+
+  #- manual via ::graph API
+  class Create < Trailblazer::Operation
+    def self.graph
+      self["__activity__"].graph
+    end
+
+    MyEnd = Class.new(Circuit::End)
+
+    step ->(options, **) { options["a"] = 1 }
+    step ->(options, **) { options["b"] = 1 }, name: "b"
+    graph.attach! target: [MyEnd.new(:myend), id: "End.myend"], edge: [Circuit::Left, {}], source: "Start.default"
+    # graph.connect! target: [B, id: :b], edge: []
+    step ->(options, **) { options["c"] = 3 }
+
+
+    task :d,
+      insert_before: "End.success",
+      outputs:       { Circuit::Right => { role: :success }, Circuit::Left => { role: :failure } }, # any outputs and their polarization, generic.
+      mappings:      { success: "End.success", failure: "End.myend" } # where do my task's outputs go?
+  end
+end
