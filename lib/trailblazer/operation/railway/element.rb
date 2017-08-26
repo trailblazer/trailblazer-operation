27df045c53ec7ea1e28c2ea7f4f3520937ff3707
Nick Sutterer <apotonick@gmail.com>
2017-08-26 17:02:46 +0200

    experimental new features: DSL::attach and DSL::element.

---
+++ lib/trailblazer/operation/railway/element.rb
@@ -0,0 +1,22 @@
+module Trailblazer
+  module Operation::Railway
+    # WARNING: The API here is still in a state of flux since we want to provide a simple yet flexible solution.
+    # This is code executed at compile-time and can be slow.
+    # @note `__sequence__` is a private concept, your custom DSL code should not rely on it.
+
+
+    # DRAFT
+    #  direction: "(output) signal"
+
+    # document ## Outputs
+    #   task/acti has outputs, role_to_target says which task output goes to what next task in the composing acti.
+
+    module DSL
+      module Element
+        def element(task_or_step, options={})
+
+        end
+      end
+    end
+  end
+end
