3d82e0051e84a6d6c8a69be33e7547d676dd8e42
Nick Sutterer <apotonick@gmail.com>
2017-08-28 15:53:26 +0200

    deprecate macros properly.

--- lib/trailblazer/operation/deprecated_macro.rb
+++ lib/trailblazer/operation/deprecated_macro.rb
@@ -4,25 +4,20 @@ module Trailblazer
     module DSL
       # Allows old macros with the `(input, options)` signature.
       module DeprecatedMacro
-        def build_task_for_macro(step:raise, **args)
-          proc, default_options, runner_options = *step
+        def add_step_or_task!(proc, *args)
+          return super unless proc.is_a?(Array)
 
-          if proc.is_a?(Proc)
-            return super if proc.arity != 2
-          else
-            return super if proc.method(:call).arity != 2
-          end
+          _proc, node_data = *proc
 
-          warn %{[Trailblazer] Macros with API (input, options) are deprecated. Please use the "Task API" signature (direction, options, flow_options). (#{proc})}
-
+          warn %{[Trailblazer] Macros with API (input, options) are deprecated. Please use the "Task API" signature (direction, options, flow_options) or use a simpler Callable. (#{proc})}
           __proc = ->(direction, options, flow_options) do
-            result    = proc.(flow_options[:exec_context], options) # run the macro, with the deprecated signature.
+            result    = _proc.(flow_options[:exec_context], options) # run the macro, with the deprecated signature.
             direction = TaskBuilder.binary_direction_for(result, Circuit::Right, Circuit::Left)
 
             [ direction, options, flow_options ]
           end
 
-          super(args.merge( step: [__proc, default_options, runner_options] ))
+          super({ task: __proc, node_data: node_data }, *args)
         end
       end # DeprecatedMacro
     end
