afbb7df3fd5706dee4917cbc7f160d86ed3ab771
Nick Sutterer <apotonick@gmail.com>
2017-08-29 10:39:42 +0200

    add test for :before with inheritance.

--- test/wire_test.rb
+++ test/wire_test.rb
@@ -185,6 +185,8 @@ class DoormatStepDocsTest < Minitest::Spec
     step :second, before: :log_success!
     step :third,  before: :log_success!
 
+    fail :log_errors!, id: "log_errors"
+
     #~ignore
     def first(options, **)
       options["row"] = [:a]
@@ -205,7 +207,9 @@ class DoormatStepDocsTest < Minitest::Spec
       options["row"] << :c
     end
 
-    fail ->(options, **) { options["row"] << :f }, id: "f"
+    def log_errors!(options, **)
+      options["row"] << :f
+    end
     #~ignore end
   end
   #:doormat-before end
@@ -213,8 +217,70 @@ class DoormatStepDocsTest < Minitest::Spec
   # it { pp F['__sequence__'].to_a }
   it { Create.({}, "b_return" => false,
                                   ).inspect("row").must_equal %{<Result:true [[:a, :b, :c, :z]] >} }
-  # it {
-  #   puts xml = Trailblazer::Diagram::BPMN.to_xml( Create["__activity__"], Create["__sequence__"] )
-  #   File.write("berry3.bpmn", xml)
-  # }
+
+    # require "trailblazer/developer"
+    # xml = Trailblazer::Diagram::BPMN.to_xml( Create["__activity__"], Create["__sequence__"] )
+
+    # token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpZCI6MywidXNlcm5hbWUiOiJhcG90b25pY2siLCJlbWFpbCI6Im5pY2tAdHJhaWxibGF6ZXIudG8ifQ."
+
+    # require "faraday"
+    # conn = Faraday.new(:url => 'https://api.trb.to')
+    # response = conn.post do |req|
+    #   req.url '/dev/v1/import'
+    #   req.headers['Content-Type'] = 'application/json'
+    #   req.headers["Authorization"] = token
+    #   require "base64"
+
+    #   req.body = %{{ "name": "Doormat/:before", "xml":"#{Base64.strict_encode64(xml)}" }}
+    # end
+
+    # puts response.status.inspect
 end
+
+class DoormatInheritanceTest < Minitest::Spec
+  #:doormat-inheritance
+  class Base < Trailblazer::Operation
+    step :log_success!
+    fail :log_errors!
+
+    #~ignore
+    # our success "end":
+    def log_success!(options, **)
+      options["row"] << :z
+    end
+
+    def log_errors!(options, **)
+      options["row"] << :f
+    end
+    #~ignore end
+  end
+  #:doormat-inheritance end
+
+  class Create < Base
+    step :first, before: :log_success!
+    step :second, before: :log_success!
+    step :third,  before: :log_success!
+
+    #~ignore
+    def first(options, **)
+      options["row"] = [:a]
+    end
+
+    # 2
+    def second(options, **)
+      options["row"] << :b
+    end
+
+    # 3
+    def third(options, **)
+      options["row"] << :c
+    end
+    #~ignore end
+  end
+  #:doormat-before end
+
+  # it { pp F['__sequence__'].to_a }
+  it { Create.({}, "b_return" => false,
+                                  ).inspect("row").must_equal %{<Result:true [[:a, :b, :c, :z]] >} }
+end
+
