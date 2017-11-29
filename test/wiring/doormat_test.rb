ee37badf239512dae8bef8543c4c3b45c44eb3b1
Nick Sutterer <apotonick@gmail.com>
2017-11-29 19:26:19 +0100

    doormat_test running.

--- test/wiring/doormat_test.rb
+++ test/wiring/doormat_test.rb
@@ -85,49 +85,49 @@ class DoormatStepDocsTest < Minitest::Spec
                                   ).inspect("row").must_equal %{<Result:true [[:a, :b, :c, :z]] >} }
 end
 
-# class DoormatInheritanceTest < Minitest::Spec
-#   #:doormatx-before-inheritance
-#   class Base < Trailblazer::Operation
-#     step :log_success!
-#     fail :log_errors!
-#     #~ignored
-#     # our success "end":
-#     def log_success!(options, **)
-#       options["row"] << :z
-#     end
+class DoormatInheritanceTest < Minitest::Spec
+  #:doormatx-before-inheritance
+  class Base < Trailblazer::Operation
+    step :log_success!
+    fail :log_errors!
+    #~ignored
+    # our success "end":
+    def log_success!(options, **)
+      options["row"] << :z
+    end
 
-#     def log_errors!(options, **)
-#       options["row"] << :f
-#     end
-#     #~ignored end
-#   end
-#   #:doormatx-before-inheritance end
+    def log_errors!(options, **)
+      options["row"] << :f
+    end
+    #~ignored end
+  end
+  #:doormatx-before-inheritance end
 
-#   #:doormat-before-inheritance-sub
-#   class Create < Base
-#     step :first, before: :log_success!
-#     step :second, before: :log_success!
-#     step :third,  before: :log_success!
-#     #~ignoredd
-#     def first(options, **)
-#       options["row"] = [:a]
-#     end
+  #:doormat-before-inheritance-sub
+  class Create < Base
+    step :first, before: :log_success!
+    step :second, before: :log_success!
+    step :third,  before: :log_success!
+    #~ignoredd
+    def first(options, **)
+      options["row"] = [:a]
+    end
 
-#     # 2
-#     def second(options, **)
-#       options["row"] << :b
-#     end
+    # 2
+    def second(options, **)
+      options["row"] << :b
+    end
 
-#     # 3
-#     def third(options, **)
-#       options["row"] << :c
-#     end
-#     #~ignoredd end
-#   end
-#   #:doormat-before-inheritance-sub end
+    # 3
+    def third(options, **)
+      options["row"] << :c
+    end
+    #~ignoredd end
+  end
+  #:doormat-before-inheritance-sub end
 
-#   # it { pp F['__sequence__'].to_a }
-#   it { Create.({}, "b_return" => false,
-#                                   ).inspect("row").must_equal %{<Result:true [[:a, :b, :c, :z]] >} }
-# end
+  # it { pp F['__sequence__'].to_a }
+  it { Create.({}, "b_return" => false,
+                                  ).inspect("row").must_equal %{<Result:true [[:a, :b, :c, :z]] >} }
+end
 
