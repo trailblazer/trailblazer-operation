d9e15fc70ece0d0aeb05d2637f2cab78bc105efc
Nick Sutterer <apotonick@gmail.com>
2018-03-03 09:57:15 +0100

    use Activity::FastTrack signals.

--- lib/trailblazer/operation/railway/fast_track.rb
+++ lib/trailblazer/operation/railway/fast_track.rb
@@ -2,8 +2,8 @@ module Trailblazer
   module Operation::Railway
     def self.fail!     ; Activity::Left  end
     def self.pass!     ; Activity::Right end
-    def self.fail_fast!; Activity::Magnetic::Builder::FastTrack::FailFast end
-    def self.pass_fast!; Activity::Magnetic::Builder::FastTrack::PassFast end
+    def self.fail_fast!; Activity::FastTrack::FailFast end
+    def self.pass_fast!; Activity::FastTrack::PassFast end
 
     module End
       FailFast = Class.new(Operation::Railway::End::Failure)
