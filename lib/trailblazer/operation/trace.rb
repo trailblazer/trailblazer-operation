fc4c2a13845cc30b80188578fd34f6de0f3cc04d
Nick Sutterer <apotonick@gmail.com>
2018-11-04 12:17:53 +0100

    simplify {Operation::Trace}.

--- lib/trailblazer/operation/trace.rb
+++ lib/trailblazer/operation/trace.rb
@@ -5,14 +5,11 @@ module Trailblazer
       def self.call(operation, *args)
         ctx = PublicCall.options_for_public_call(*args) # redundant with PublicCall::call.
 
-        # Prepare the tracing-specific arguments. This is only run once for the entire circuit!
-        operation, *args = Trailblazer::Activity::Trace.arguments_for_call(operation, [ctx, {}], {})
+        stack, signal, (ctx, flow_options) = Activity::Trace.(operation, [ctx, {}])
 
-        last_signal, (ctx, flow_options) = Activity::TaskWrap.invoke(operation, *args)
+        result = Railway::Result(signal, ctx) # redundant with PublicCall::call.
 
-        result = Railway::Result(last_signal, ctx) # redundant with PublicCall::call.
-
-        Result.new(result, flow_options[:stack].to_a)
+        Result.new(result, stack.to_a)
       end
 
       # `Operation::trace` is included for simple tracing of the flow.
