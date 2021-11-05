fa223cf4e1faa9a47e260206c6fe7ba8ddbf5179
Nick Sutterer <apotonick@gmail.com>
2021-11-05 11:03:00 +0100

    more class dependencies tests.

--- test/class_dependencies_test.rb
+++ test/class_dependencies_test.rb
@@ -8,16 +8,44 @@ class ClassDependenciesTest < Minitest::Spec
 
     self["model.class"] = Module
 
-    step ->(options, **) { options["a"] = options["model.class"] }
+    step ->(ctx, **) { ctx["a"] = ctx["model.class"] }
   end
 
   it { Index.({}).inspect("a", "model.class").must_equal %{<Result:true [Module, Module] >} }
 
   it "creates separate ctx for circuit interface" do
+
+    raise "this can't work since the OP is run without TaskWrap.invoke"
     signal, (ctx, _) = Index.([{}, {}], {})
 
-    ctx["model.class"].inspect.must_equal %{Module} # FIXME: should this be here?
     ctx[:a].inspect.must_equal %{Module}
+    ctx["model.class"].inspect.must_equal %{Module} # FIXME: should this be here?
+  end
+
+# nested OPs
+  it "injects class dependencies for nested OP" do
+    class Home < Trailblazer::Operation
+      step Subprocess(Index)
+    end
+
+    # "model.class" gets injected automatically just before {Index}.
+    Home.({params: {}}).inspect.must_equal %{<Result:true #<Trailblazer::Context::Container wrapped_options={:params=>{}, \"model.class\"=>Module} mutable_options={\"a\"=>Module}> >}
+
+    # "model.class" gets injected by user and overrides class dependencies.
+    Home.({params: {}, "model.class" => Symbol}).inspect.must_equal %{<Result:true #<Trailblazer::Context::Container wrapped_options={:params=>{}, :\"model.class\"=>Symbol, \"model.class\"=>Symbol} mutable_options={\"a\"=>Symbol}> >}
+
+
+    class Dashboard < Trailblazer::Operation
+      extend ClassDependencies
+      self["model.class"] = Float # this overrides {Index}'es dependency
+
+      pass ->(ctx, **) { ctx[:Dashboard] = ctx["model.class"] }
+      step Subprocess(Index)
+    end
+
+  # "model.class" gets injected automatically in {Dashboard} and overrides the {Index} input.
+    Dashboard.({params: {}}).inspect.must_equal %{<Result:true #<Trailblazer::Context::Container wrapped_options={:params=>{}, \"model.class\"=>Float} mutable_options={\"a\"=>Module}> >}
+
   end
 
   describe "inheritance" do
