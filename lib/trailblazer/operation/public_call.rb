5a4a9938bff1e93e8a2c44d3338ebf831e27cc86
Nick Sutterer <apotonick@gmail.com>
2025-03-27 10:41:17 +0100

    better comments.

--- lib/trailblazer/operation/public_call.rb
+++ lib/trailblazer/operation/public_call.rb
@@ -1,32 +1,33 @@
 module Trailblazer
   module Operation::PublicCall
     # TODO: add docs from original {Operation.call}.
-    def call(options = {}, flow_options = {}, **circuit_options)
+    def call(options = {}, flow_options = {}, **circuit_options, &block)
       return strategy_call(options, **circuit_options) if options.is_a?(Array) # This is kind of a hack that could be well hidden if Ruby had method overloading. Goal is to simplify the call thing as we're fading out Operation::public_call anyway.
 
       # DISCUSS: move to separate method?
       # normalize options:
       options = options.merge(circuit_options) # when using Op.call(params:, ...), {circuit_options} will always be ctx variables.
 
-      invoke_with_public_interface(options)
+      invoke_with_public_interface(options, &block)
     end
 
-    # TODO: we always use {self.__} as a canonical invoke.
-    def invoke_with_public_interface(options, **options_for_invoke)
-      # Only use {#__} on the top level.
-      signal, (ctx, flow_options) = self.__(self, options, initial_wrap_static: INITIAL_WRAP_STATIC, **options_for_invoke) # Operation.__ is defined via {trailblazer-invoke}. It's the "canonical invoke".
+    def invoke_with_public_interface(options, **options_for_invoke, &block)
+      # On the top level, use {#__}.
+      options_for_invoke = {matcher_context: block.binding.receiver}.merge(options_for_invoke) if block # DISCUSS: do we always want that?
+
+      signal, (ctx, flow_options) = self.__(self, options, initial_wrap_static: INITIAL_WRAP_STATIC, **options_for_invoke, &block) # Operation.__ is defined via {trailblazer-invoke}. It's the "canonical invoke".
 
       Operation::Railway::Result(signal, ctx, flow_options)
     end
 
+    # NOTE: mostly copied from {Activity::TaskWrap.call_task}.
+    #
     # This TaskWrap step replaces the default {call_task} step for this very operation.
     # Instead of invoking the operation using {Operation.call}, it does {Operation.call_with_circuit_interface},
     # so we don't invoke {Operation.call} twice.
     #
-    # I don't really like this "hack", but it's the only way until we get method overloading.
-    #
     # @private
-    def self.call_operation_with_circuit_interface(wrap_ctx, original_args) # DISCUSS: copied from {TaskWrap.call_task}.
+    def self.call_operation_with_circuit_interface(wrap_ctx, original_args)
       operation = wrap_ctx[:task]
 
       original_arguments, original_circuit_options = original_args
